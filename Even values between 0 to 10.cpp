dc052592191a78356a18af88e387b12cc90fc324
Prathamesh <80304812+PageNotFound222@users.noreply.github.com>
2025-07-27 18:21:47 +0530

    Create Even values between 0 to 10.cpp

---
+++ Even values between 0 to 10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i <= 10; i += 2) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
+
+/*
+Output:
+0
+2
+4
+6
+8
+10
+*/
