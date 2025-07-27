54f255007f4cce3b4d022afd6fca2fb36b7de07e
Prathamesh <80304812+PageNotFound222@users.noreply.github.com>
2025-07-27 18:31:19 +0530

    Create print a word using for loop.cpp

---
+++ print a word using for loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Loop to print "SIT" 5 times
+    for (int i = 1; i <= 5; i++) {
+        cout << "SIT" << endl;
+    }
+
+    return 0;
+}
+
+/* OUTPUT:
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
