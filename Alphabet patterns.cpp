0cceb1b8b7f2500d0820731170a83276dad5f2d8
Prathamesh <80304812+PageNotFound222@users.noreply.github.com>
2025-07-27 18:51:10 +0530

    Update Alphabet patterns.cpp

--- Alphabet patterns.cpp
+++ Alphabet patterns.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     char ch = 'A';  
     for (int i = 1; i <= 5; i++) {
-        // Inner loop for characters in each row
+       
         for (int j = 1; j <= i; j++) {
             cout << ch << " ";
             ch++; 
